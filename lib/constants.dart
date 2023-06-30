eadc869de06972713c66e99bf5a06544ca4bbc4a
Vinuka Vinnath <wlvvinnath@gmail.com>
2023-07-01 00:40:09 +0530

    Textfields designed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+//Main Colors
+const kDeepBlueColor=Color(0xFF16056B);
+const kAmberColor=Color(0xFFFD9519);
+const kOceanBlueColor=Color(0xFF5696FA);
+const kLightBlueColor=Color(0xFFB4D7FE);
+const kAshWhiteColor=Color(0xFFF4F7F9);
+const kDarkGreyColor=Color(0xFF4B4646);
+
+//Additional Colors
+const kBrilliantWhite= Color(0xFFFFFFFF);
+const kJetBlack= Color(0xFF000000);
