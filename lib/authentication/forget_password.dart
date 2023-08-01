4afb9c6d2b26c689c5566ec26b4582ea28c9f48a
Vinuka Vinnath <wlvvinnath@gmail.com>
2023-08-01 21:40:14 +0530

    Revert "Forgot Password page and Functionality completed"
    This reverts commit d1f4e7fee42036bcbbc02be970d1ea79bda4928d.

--- lib/authentication/forget_password.dart
+++ lib/authentication/forget_password.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:taskmate/authentication/log_in.dart';
 
 import 'package:taskmate/constants.dart';
 
@@ -30,48 +29,9 @@ class _ForgetPasswordState extends State<ForgetPassword> {
       );
       showDialog(
         context: context,
-        barrierDismissible: false,
         builder: (context) {
           return AlertDialog(
-            title: const Image(
-              image: AssetImage('images/done.png'),
-              width: 150,
-              height: 150,
-            ),
-            content: const Text(
-              'Reset Link was successfully sent! Check your Inbox.',
-              textAlign: TextAlign.center,
-            ),
-            actions: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const Login(),
-                    ),
-                  );
-                },
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                  backgroundColor: MaterialStateProperty.all(kDeepBlueColor),
-                ),
-                child: const Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-                  child: Text('Back to Login'),
-                ),
-              ),
-            ],
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              side: const BorderSide(color: kDeepBlueColor,width: 2.0),
-            ),
-            elevation: 20.0,
-            actionsAlignment: MainAxisAlignment.center,
+            content: Text('Reset Link was successfully sent!'),
           );
         },
       );
@@ -116,70 +76,72 @@ class _ForgetPasswordState extends State<ForgetPassword> {
             ),
           ),
         ),
-        child: ListView(
+        child: Column(
           children: <Widget>[
-            const Image(
-              image: AssetImage('images/forgot_password.png'),
-              height: 350.0,
-              width: 350.0,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text('Did you forget password?',
-                  textAlign: TextAlign.center, style: kHeadingTextStyle),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Form(
-                key: _formKey,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 28.0),
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    color: kBrilliantWhite,
-                    borderRadius: BorderRadius.circular(20.0),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  const Image(
+                    image: AssetImage('images/forgot_password.jpg'),
                   ),
-                  child: TextFormField(
-                    controller: _forgetPasswordController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the Email';
-                      }
-                      return null; // Return null for valid input
-                    },
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Enter your email here',
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Did you forget password?',
+                        textAlign: TextAlign.center, style: kHeadingTextStyle),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 28.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      decoration: BoxDecoration(
+                        color: kBrilliantWhite,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: TextFormField(
+                        controller: _forgetPasswordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the Email';
+                          }
+                          return null; // Return null for valid input
+                        },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your email here',
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Container(
-              margin:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 28.0),
-              width: screenWidth,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: kDeepBlueColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 28.0),
+                    width: screenWidth,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: kDeepBlueColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          forgetPassword();
+                          // Form is valid, proceed with submission or other actions
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          'Send Recovery Link',
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    forgetPassword();
-                    // Form is valid, proceed with submission or other actions
-                  }
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Send Recovery Link',
-                    style: TextStyle(fontSize: 15.0),
-                  ),
-                ),
+                ],
               ),
             ),
           ],
