b39b4b6eda287e83ab8a53506fefa63fcd9db78b
Vinuka Vinnath <wlvvinnath@gmail.com>
2023-08-22 22:23:02 +0530

    Initial commit of live branch

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class Taskmate extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Poppins"),
       home: const SafeArea(
-        child: HomePage(),
+        child: ProfileFreelancer(),
       ),
     );
   }
