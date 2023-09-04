e60036b262c18332ea5c5ae02fa6732d38b8a26e
Vinuka Vinnath <wlvvinnath@gmail.com>
2023-09-05 02:26:32 +0530

    Dashboard directory deleted

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class Taskmate extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Poppins"),
       home: const SafeArea(
-        child: ProfileClient(),
+        child: HomePage(),
 
       ),
     );
