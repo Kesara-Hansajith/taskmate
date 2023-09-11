b623693f546260f6e807bfbe11d2769e865f9539
basuru07 <yasaruwanbasuru637@gmail.com>
2023-09-11 23:40:05 +0530

    Post a Job

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:taskmate/client_home_page.dart';
 
 import 'package:get/get_core/src/get_main.dart';
 import 'package:taskmate/authentication/root_page.dart';
+import 'package:taskmate/pages/client/client_post_job.dart';
 import 'package:taskmate/pages/freelancer/proposals/pending_jobs_pages/pending_jobs.dart';
 
 import 'package:taskmate/profile/client/profile_client.dart';
@@ -58,7 +59,7 @@ class Taskmate extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Poppins"),
       home: const SafeArea(
-        child: ProfileClient(),
+        child: ClientPostJob(),
       ),
     );
   }
--- lib/pages/freelancer/proposals/active_jobs_pages/reviews_section.dart
+++ lib/pages/freelancer/proposals/active_jobs_pages/reviews_section.dart
@@ -1,131 +1,131 @@
-import 'package:flutter/material.dart';
-import 'package:taskmate/components/dark_main_button.dart';
-import 'package:taskmate/components/snackbar.dart';
-import 'package:taskmate/constants.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-
-class Reviews extends StatefulWidget {
-  const Reviews({super.key});
-
-  @override
-  State<Reviews> createState() => _ReviewsState();
-}
-
-class _ReviewsState extends State<Reviews> {
-  final _formKey = GlobalKey<FormState>();
-  //Variable for star count
-  int _starRating = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
-    final reviewFieldController = TextEditingController();
-
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SizedBox(
-          width: screenWidth,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: Text(
-                  'Write Your Review',
-                  style: kJobCardTitleTextStyle.copyWith(color: kJetBlack),
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: reviewFieldController,
-                  maxLines: 6,
-                  decoration: InputDecoration(
-                    hintText: 'Write a review about your Client.',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        width: 1.0,
-                        color: kDarkGreyColor,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        width: 1.0,
-                        color: kDeepBlueColor,
-                      ),
-                    ),
-                    filled: true,
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Field can\'t be empty';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: Text(
-                  'Rate with Stars!',
-                  style: kJobCardTitleTextStyle.copyWith(color: kJetBlack),
-                ),
-              ),
-              Center(
-                child: Column(
-                  children: <Widget>[
-                    RatingBar.builder(
-                      initialRating: 0,
-                      minRating: 1,
-                      direction: Axis.horizontal,
-                      allowHalfRating: false,
-                      itemCount: 5,
-                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      itemBuilder: (context, _) => const Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      onRatingUpdate: (rating) {
-                        setState(() {
-                          _starRating = rating.toInt();
-                        });
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Text(
-                        (_starRating == 1
-                            ? 'You offered $_starRating Star!'
-                            : 'You offered $_starRating Stars!'),
-                        style: kJobCardTitleTextStyle,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              DarkMainButton(
-                  title: 'Submit Review',
-                  process: () {
-                    if (_formKey.currentState!.validate()) {}
-                    if (_starRating == 0) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        CustomSnackBar('Please rate the user with stars.'),
-                      );
-                    } else {}
-                    //TODO Submit review functionality
-                  },
-                  screenWidth: screenWidth)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:taskmate/components/dark_main_button.dart';
+// import 'package:taskmate/components/snackbar.dart';
+// import 'package:taskmate/constants.dart';
+// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+//
+// class Reviews extends StatefulWidget {
+//   const Reviews({super.key});
+//
+//   @override
+//   State<Reviews> createState() => _ReviewsState();
+// }
+//
+// class _ReviewsState extends State<Reviews> {
+//   final _formKey = GlobalKey<FormState>();
+//   //Variable for star count
+//   int _starRating = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     double screenWidth = MediaQuery.of(context).size.width;
+//
+//     final reviewFieldController = TextEditingController();
+//
+//     return SingleChildScrollView(
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 24.0),
+//         child: SizedBox(
+//           width: screenWidth,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(vertical: 24.0),
+//                 child: Text(
+//                   'Write Your Review',
+//                   style: kJobCardTitleTextStyle.copyWith(color: kJetBlack),
+//                 ),
+//               ),
+//               Form(
+//                 key: _formKey,
+//                 child: TextFormField(
+//                   controller: reviewFieldController,
+//                   maxLines: 6,
+//                   decoration: InputDecoration(
+//                     hintText: 'Write a review about your Client.',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                       borderSide: const BorderSide(
+//                         width: 1.0,
+//                         color: kDarkGreyColor,
+//                       ),
+//                     ),
+//                     focusedBorder: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                       borderSide: const BorderSide(
+//                         width: 1.0,
+//                         color: kDeepBlueColor,
+//                       ),
+//                     ),
+//                     filled: true,
+//                   ),
+//                   validator: (value) {
+//                     if (value!.isEmpty) {
+//                       return 'Field can\'t be empty';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(vertical: 24.0),
+//                 child: Text(
+//                   'Rate with Stars!',
+//                   style: kJobCardTitleTextStyle.copyWith(color: kJetBlack),
+//                 ),
+//               ),
+//               Center(
+//                 child: Column(
+//                   children: <Widget>[
+//                     RatingBar.builder(
+//                       initialRating: 0,
+//                       minRating: 1,
+//                       direction: Axis.horizontal,
+//                       allowHalfRating: false,
+//                       itemCount: 5,
+//                       itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+//                       itemBuilder: (context, _) => const Icon(
+//                         Icons.star,
+//                         color: Colors.amber,
+//                       ),
+//                       onRatingUpdate: (rating) {
+//                         setState(() {
+//                           _starRating = rating.toInt();
+//                         });
+//                       },
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric(vertical: 8.0),
+//                       child: Text(
+//                         (_starRating == 1
+//                             ? 'You offered $_starRating Star!'
+//                             : 'You offered $_starRating Stars!'),
+//                         style: kJobCardTitleTextStyle,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 30.0,
+//               ),
+//               DarkMainButton(
+//                   title: 'Submit Review',
+//                   process: () {
+//                     if (_formKey.currentState!.validate()) {}
+//                     if (_starRating == 0) {
+//                       ScaffoldMessenger.of(context).showSnackBar(
+//                         CustomSnackBar('Please rate the user with stars.'),
+//                       );
+//                     } else {}
+//                     //TODO Submit review functionality
+//                   },
+//                   screenWidth: screenWidth)
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
