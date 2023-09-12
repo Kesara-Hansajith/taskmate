2cba156d9a25a22e5b8267baab7865676498f698
basuru07 <yasaruwanbasuru637@gmail.com>
2023-09-12 20:56:00 +0530

    Job

--- lib/pages/client/jobs/pending/client_pending_job_card.dart
+++ lib/pages/client/jobs/pending/client_pending_job_card.dart
@@ -7,82 +7,69 @@ import 'package:taskmate/pages/freelancer/proposals/active_jobs_pages/active_job
 
 class ClientPendingJobCard extends StatefulWidget {
   const ClientPendingJobCard({
-    super.key,
     required this.documentID,
+    required this.data,
   });
 
   final String documentID;
+  final Map<String, dynamic> data;
 
   @override
   State<ClientPendingJobCard> createState() => _ClientPendingJobCardState();
 }
 
 class _ClientPendingJobCardState extends State<ClientPendingJobCard> {
-  CollectionReference projects =
-  FirebaseFirestore.instance.collection('client_pending_jobs');
-
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return FutureBuilder<DocumentSnapshot>(
-      future: projects.doc(widget.documentID).get(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-          snapshot.data!.data() as Map<String, dynamic>;
-          //Overall Active Job Card flows through here
-          return InkWell(
-            onTap: () {
-              // Navigator.of(context).push(
-              //   MaterialPageRoute(
-              //     builder: (context) => ClientPendingJobDetails(
-              //       documentID: widget.documentID,
-              //     ),
-              //   ),
-              // );
-            },
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 4.0),
-              padding:
-              const EdgeInsets.symmetric(vertical: 16.0, horizontal: 9.0),
-              width: screenWidth,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16.0),
-                border: Border.all(color: kDeepBlueColor, width: 1.0),
+    // Check if widget.data is not null and is of type Map<String, dynamic>
+    if (widget.data != null && widget.data is Map<String, dynamic>) {
+      final data = widget.data as Map<String, dynamic>;
+      return InkWell(
+        onTap: () {
+          // Handle onTap action if needed
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 4.0),
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 9.0),
+          width: screenWidth,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+            border: Border.all(color: kDeepBlueColor, width: 1.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '${data['jobTitle']}',
+                style: kJobCardTitleTextStyle,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '${data['title']}',
-                    style: kJobCardTitleTextStyle,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Price Range: LKR.${data['priceRange']}',
-                          style: kJobCardDescriptionTextStyle,
-                        ),
-                        Text(
-                          'Job Done within: ${data['dayCount']}',
-                          style: kJobCardDescriptionTextStyle,
-                        ),
-                      ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Price Range: LKR.${data['budget']}',
+                      style: kJobCardDescriptionTextStyle,
                     ),
-                  ),
-                ],
+                    Text(
+                      'Job Done within: ${data['dayCount']}',
+                      style: kJobCardDescriptionTextStyle,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        }
-        return const Center(
-          child: Text('Loading....'),
-        );
-      },
-    );
+            ],
+          ),
+        ),
+      );
+    } else {
+      // Handle the case when data is null or not of the expected type
+      return const Center(
+        child: Text('Data not available'),
+      );
+    }
   }
 }
