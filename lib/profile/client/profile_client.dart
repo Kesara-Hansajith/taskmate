1fd2c8562dd00046b38b7c6b778d7710953fdfcc
Vinuka Vinnath <wlvvinnath@gmail.com>
2023-12-20 20:56:59 +0530

    Email fields removed from both actors when signing up

--- lib/profile/client/profile_client.dart
+++ lib/profile/client/profile_client.dart
@@ -41,6 +41,7 @@ class _ProfileClientState extends State<ProfileClient> {
   String? selectedSkills;
   bool dataSubmitted = false;
   bool _isPasswordVisible = false;
+  String useremail = "";
 
   get emailRegex => RegExp(
         r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
@@ -69,6 +70,14 @@ class _ProfileClientState extends State<ProfileClient> {
     if (picked != null && picked != DateTime.now()) {
       final formattedDate = DateFormat('yyyy-MM-dd').format(picked);
       birthdayController.text = formattedDate; // Update the text field
+    }
+  }
+
+  void getCurrentUserEmail() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    if (user != null) {
+      useremail = user.email!;
     }
   }
 
@@ -213,7 +222,7 @@ class _ProfileClientState extends State<ProfileClient> {
         city: cityController.text.trim(),
         phoneNo: phoneController.text.trim(),
         profilePhotoUrl: profileImageUrl,
-        email: emailController.text.trim(),
+        email: useremail,
         // password: passwordController.text.trim(),
         professionalrole: professionalroleController.text.trim(),
       );
@@ -239,6 +248,12 @@ class _ProfileClientState extends State<ProfileClient> {
         // You may want to display an error message or redirect the user to the login page
       }
     }
+  }
+
+  @override
+  void initState() {
+    getCurrentUserEmail();
+    super.initState();
   }
 
   @override
@@ -402,47 +417,6 @@ class _ProfileClientState extends State<ProfileClient> {
                   ),
                   const SizedBox(
                     height: 6.0,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const UserDataGatherTitle(title: 'Email*'),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: TextFormField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(10.0),
-                            hintText: 'Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                width: 1.0,
-                                color: kDarkGreyColor,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: const BorderSide(
-                                width: 2.0,
-                                color: kDeepBlueColor,
-                              ),
-                            ),
-                            filled: true,
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter your Email';
-                            } else if (!RegExp(
-                                    r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
-                                .hasMatch(value)) {
-                              return 'Please enter a valid email address';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ],
                   ),
                   const UserDataGatherTitle(title: 'Address*'),
                   Padding(
